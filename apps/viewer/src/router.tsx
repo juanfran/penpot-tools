7e08a0cf12420e71a7f3ebeed0cab73ea7d21ab3
Juanfran <juanfran.ag@gmail.com>
2026-04-14 21:21:18 +0200

    wip

---
+++ apps/viewer/src/router.tsx
@@ -0,0 +1,28 @@
+import { createRouter as createTanStackRouter } from '@tanstack/react-router';
+import { routeTree } from './routeTree.gen';
+import { setupRouterSsrQueryIntegration } from '@tanstack/react-router-ssr-query';
+import { QueryClient } from '@tanstack/react-query';
+
+export function getRouter() {
+  const queryClient = new QueryClient();
+
+  const router = createTanStackRouter({
+    routeTree,
+    context: { queryClient },
+    defaultPreload: 'intent',
+    scrollRestoration: true,
+    defaultPreloadStaleTime: 0,
+  });
+  setupRouterSsrQueryIntegration({
+    router,
+    queryClient,
+  });
+
+  return router;
+}
+
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: ReturnType<typeof getRouter>;
+  }
+}
