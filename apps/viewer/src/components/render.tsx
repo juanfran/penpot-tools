cfbe73013b81b050ed2fd9caabfef7cf8192fa1f
Juanfran <juanfran.ag@gmail.com>
2026-04-16 18:47:00 +0200

    wip

--- apps/viewer/src/components/render.tsx
+++ apps/viewer/src/components/render.tsx
@@ -1,5 +1,6 @@
 import { getPageHtmlFn } from '#/lib/server/penpot-api';
 import { queryOptions, useSuspenseQuery } from '@tanstack/react-query';
+import { TransformWrapper, TransformComponent } from 'react-zoom-pan-pinch';
 
 export const getPageHtmlOptions = (fileId: string, pageId: string) => {
   return queryOptions({
@@ -23,10 +24,24 @@ export const Render = ({ pageId, fileId }: { pageId: string; fileId: string }) =
         </>
       )}
       {data.tokensCss && <style>{data.tokensCss}</style>}
-      <div
-        className="h-full w-full contain-strict"
-        dangerouslySetInnerHTML={{ __html: data.html }}
-      />
+
+      <div className="h-full w-full contain-strict">
+        <TransformWrapper
+          minScale={0.05}
+          maxScale={10}
+          limitToBounds={false}
+          centerOnInit={true}
+          smooth={false}
+          wheel={{ step: 0.1 }}
+        >
+          <TransformComponent wrapperStyle={{ width: '100%', height: '100%' }}>
+            <div
+              className="pointer-events-none relative h-[3000px] w-[3000px]"
+              dangerouslySetInnerHTML={{ __html: data.html }}
+            />
+          </TransformComponent>
+        </TransformWrapper>
+      </div>
     </>
   );
 };
